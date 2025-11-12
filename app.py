ec857eb2bc356e96d9f5a86bd7206db681ee2c29
carlosgg0 <carlosgarciag552@gmail.com>
2025-11-13 00:03:41 +0100

    Arreglando el punto de escucha /detectar

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, request
+import joblib
 from redis import Redis, RedisError
 import os
 import socket
+import keras
+from keras.models import Sequential
+from keras.saving import load_model
+import numpy as np
+import pandas as pd
 
+
+WINDOW_SIZE = 24
 
 PORT = os.getenv("PORT", 80)
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
@@ -13,6 +21,14 @@ redis.flushdb()
 
 # Create Flask application 
 app = Flask(__name__)
+
+# Load the model and the scaler
+model = load_model("modelo.keras")
+scaler = joblib.load("scaler.pkl")
+
+# The array in which we will store exactly WINDOW_SIZE measurements
+# to make predictions
+measures = []
 
 
 @app.route("/")
@@ -55,6 +71,30 @@ def listar():
         print(err)
         return "<i> There's no data to display! </i>"
 
+
+@app.route("/detectar", methods=["GET"])
+def detectar():
+    valor = request.args.get('dato')
+    try:
+        redis.ts().add(key="values", timestamp='*', value=valor)
+    except RedisError:
+        return "<i> Something went bad, please try again </i>"
+    
+    size = redis.ts().info(key="values")["total_samples"]
+    last_timestamp = redis.ts().get(key="values")[0]
+    
+    print("Size:", size)
+    print("Last timestamp:", last_timestamp)
+
+    if size <= WINDOW_SIZE:
+        measures = np.append(measures, valor)
+        return "<i> There are not enough measures! </i>"
+    else:  
+        measures = measures[-WINDOW_SIZE : ]
+        print(measures.shape)
+        prediction = model.predict(measures)
+        return str(prediction)
+    
 
 def main():
     app.run(host="0.0.0.0", port=PORT)
