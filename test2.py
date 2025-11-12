eddd6355a03b98237420b39dee5e6a8cb1aa5168
carlosgg0 <carlosgarciag552@gmail.com>
2025-11-13 00:03:59 +0100

    Añadidos los modelos

---
+++ test2.py
@@ -0,0 +1,20 @@
+from time import sleep
+import requests
+import random
+
+BASE_URL = "http://localhost/"
+N = 100
+
+while True:
+    try: 
+        val = random.random()
+        print("Introduciendo", val, "...")
+        r = requests.get(BASE_URL + f"/detectar?dato={val}")
+        print(r.content)
+        sleep(1)
+    except ConnectionError:
+        print("Hubo un error")
+        exit(-1)
+    except KeyboardInterrupt:
+        print("\nSe ha interrumpido el programa")
+        exit(0)
